52929036a87447283d5d14b5483457edcc86145b
Enoch Ezeani <40115382+eizodann@users.noreply.github.com>
2020-01-15 14:08:24 +0100

    services & dependency Injection

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,19 +8,17 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>()
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'Simply a recipe', 'https://c.pxhere.com/photos/b6/a1/esfiha_kibe_tidbits_snacks_power_supply-1375826.jpg!d'),
-    new Recipe('A Test 2 Recipe', 'Simply 2 a recipe', 'https://c.pxhere.com/photos/b6/a1/esfiha_kibe_tidbits_snacks_power_supply-1375826.jpg!d')
-  ];
 
-  constructor() { }
+  recipes: Recipe[];
+
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe)
-  }
+  // onRecipeSelected(recipe: Recipe) {
+  //   this.recipeWasSelected.emit(recipe)
+  // }
 
 }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,41 @@
+import { Injectable, EventEmitter, Output } from '@angular/core';
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>()
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe', 
+      'Simply a recipe', 
+      'https://c.pxhere.com/photos/b6/a1/esfiha_kibe_tidbits_snacks_power_supply-1375826.jpg!d',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]
+      ),
+    new Recipe(
+      'A Test 2 Recipe', 
+      'Simply 2 a recipe', 
+      'https://c.pxhere.com/photos/b6/a1/esfiha_kibe_tidbits_snacks_power_supply-1375826.jpg!d',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Carrot', 7)
+      ])
+  ];
+
+  constructor(private slService: ShoppingListService) { }
+
+  getRecipes() {
+    return this.recipes.slice()
+  }
+
+  addIngredientsToShoppingList (ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+}
